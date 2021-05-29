0e14aa1680e6efb32d0977654af909ca39eacbf2
Jie <mjunzh@gmail.com>
2021-05-29 23:26:06 +0800

    Update profile page

--- lib/pages/profile/profile.dart
+++ lib/pages/profile/profile.dart
@@ -1,51 +1,53 @@
 import "package:flutter/material.dart";
 import 'package:gitme/components/profile/profile_info.dart';
+import 'package:gitme/pages/profile/follow.dart';
+import 'package:gitme/pages/repo.dart';
 
 class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       child: Scaffold(
-        body: CustomScrollView(
-          slivers: <Widget>[
-            SliverAppBar(
-              title: Text("Jie"),
-              pinned: true,
-              expandedHeight: 250.0,
-              flexibleSpace: FlexibleSpaceBar(
-                background: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: <Widget>[
-                    ProfileInfo(
-                      avatarUrl: "https://placekitten.com/200/300",
-                      name: "Jie",
-                      location: "New Taipei City, Taiwan",
-                    ),
-                    SizedBox(height: 8.0),
-                    TabBar(
-                      labelPadding: EdgeInsets.zero,
-                      tabs: <Widget>[
-                        Tab(text: "Repos"),
-                        Tab(text: "Stars"),
-                        Tab(text: "Followers"),
-                        Tab(text: "Following"),
-                      ],
-                    ),
+        body: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              SliverAppBar(
+                title: Text("Jie"),
+                pinned: true,
+                expandedHeight: 250.0,
+                flexibleSpace: FlexibleSpaceBar(
+                  background: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      SizedBox(height: 16.0),
+                      ProfileInfo(
+                        avatarUrl: "https://placekitten.com/200/300",
+                        name: "Jie",
+                        location: "New Taipei City, Taiwan",
+                      ),
+                    ],
+                  ),
+                ),
+                bottom: TabBar(
+                  labelPadding: EdgeInsets.zero,
+                  tabs: <Widget>[
+                    Tab(text: "Repos"),
+                    Tab(text: "Stars"),
+                    Tab(text: "Followers"),
+                    Tab(text: "Following"),
                   ],
                 ),
               ),
-            ),
-            SliverFillRemaining(
-              child: TabBarView(
-                children: <Widget>[
-                  Text("Repos"),
-                  Text("Stars"),
-                  Text("Followers"),
-                  Text("Following"),
-                ],
-              ),
-            ),
-          ],
+            ];
+          },
+          body: TabBarView(
+            children: [
+              RepoPage(),
+              RepoPage(),
+              FollowPage(),
+              FollowPage(),
+            ],
+          ),
         ),
       ),
       length: 4,
