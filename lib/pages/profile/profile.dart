f6eb8c1ccae46c7616c73e4f07cba76ceebc27f9
Jie <mjunzh@gmail.com>
2021-07-08 23:56:06 +0800

    Apply provider for state management

--- lib/pages/profile/profile.dart
+++ lib/pages/profile/profile.dart
@@ -3,17 +3,20 @@ import 'package:gitme/components/profile/profile_info.dart';
 import 'package:gitme/pages/profile/follow.dart';
 import 'package:gitme/pages/profile/start.dart';
 import 'package:gitme/pages/repo.dart';
+import 'package:gitme/stores/account.dart';
+import 'package:provider/provider.dart';
 
 class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var account = Provider.of<AccountModel>(context);
     return DefaultTabController(
       child: Scaffold(
         body: NestedScrollView(
           headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
             return <Widget>[
               SliverAppBar(
-                title: Text("Jie"),
+                title: Text(account.profile.login!),
                 pinned: true,
                 expandedHeight: 250.0,
                 flexibleSpace: FlexibleSpaceBar(
@@ -22,9 +25,10 @@ class ProfilePage extends StatelessWidget {
                     children: <Widget>[
                       SizedBox(height: 16.0),
                       ProfileInfo(
-                        avatarUrl: "https://placekitten.com/200/300",
-                        name: "Jie",
-                        location: "New Taipei City, Taiwan",
+                        avatarUrl: account.profile.avatarUrl!,
+                        name: account.profile.name!,
+                        bio: account.profile.bio ?? '',
+                        location: account.profile.location ?? '',
                       ),
                     ],
                   ),
